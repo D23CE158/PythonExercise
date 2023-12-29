0bd53be8370e36fd53e10ebab9a8b5e3111d9e76
YashGhoda <yashaghoda@gmail.com>
2023-12-29 15:39:28 +0530

    Dictionary Exercise with Comments

---
+++ DictionaryTask/Exercise1.py
@@ -0,0 +1,31 @@
+dog = {}
+dog = {
+    'name':'puppy',
+    'color':'black',
+    'breed':'lab',
+    'legs':'black',
+    'age':5
+}
+student = {
+    'f_name':'namya',
+    'l_name':'oza',
+    'gender':'male',
+    'age':18,
+    'marital_status':'non_married',
+    'skills':'playing tabla',
+    'country':'India',
+    'city':'Junagadh',
+    'address':'Vanzari chowk'
+}
+print(len(student))
+print(student.get('skills'))
+l = list(student)       #convert dictionary to list
+print(type(l))
+student['skills'] = 'playing vollyball'     #modify skill
+print(student.get('skills'))
+print([student.keys()])     #get all keys as a list
+print([student.values()])   #get all values as a list
+print(student.items())      #get all items as a list of tuples
+print(student.pop('address'))   #delete address
+del dog     #delete dog dictionary
+
