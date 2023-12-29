7d96a94b55e9b0168ff000cfcfedce627f280202
YashGhoda <yashaghoda@gmail.com>
2023-12-29 14:48:50 +0530

    Tuples Exercise1

---
+++ Tupletask/Exercise1.py
@@ -0,0 +1,9 @@
+t = ()
+brother = ('nehit','prayag','harshal','vismay')
+sister = ('kamna','shruti','yesha')
+siblings = brother + sister     #join brother and sister
+print(len(siblings))    #length of tuple
+father = 'Amitbhai'
+mother = 'Meetaben'
+family_members = siblings + (father,mother)     #add mother and father name in tuple
+print(family_members)
